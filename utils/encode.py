adc701d63f8715a8fe096f76cfa108fbebab548a
Mohamed Deraz Nasr <mderaznasr@gmail.com>
2025-11-06 19:14:00 -0500

    Update encode.py

--- utils/encode.py
+++ utils/encode.py
@@ -1,1 +1,13 @@
-#here we are convert
+'''
+Each protein is a string of amino-acid letters:
+- but neural networks can't process letters
+They need numbers (tensors) as input
+
+here we convert each amino acids letter into a numeric token ID
+and pads all sequences to the same length
+which makes them usable by Pytorch
+
+Why Encode?
+
+'''
+
